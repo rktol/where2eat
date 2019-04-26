8ce49c7ed35c1fcffced4a90a422a4452afc6ba5
Rikuto Iitsuka <s1711426@s.tsukuba.ac.jp>
2019-04-26 17:18:16 +0900

    ルートパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-			      get 'lunch/go',to: 'lunch#go'
+			      get 'lunch/go'x
+			      root 'lunch#go'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
