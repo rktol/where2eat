56aea525efdd34e4d6fcaca85526ea0313ca8dbf
Rikuto Iitsuka <s1711426@s.tsukuba.ac.jp>
2019-05-17 16:12:39 +0900

    nameのみを登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'eateries/new'
+  post 'eateries/create'
 			      get 'lunch/go'
 			      root 'lunch#go'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
