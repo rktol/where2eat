f915093ccb668248bdfee2c5f5a283eb784cd137
Rikuto Iitsuka <s1711426@s.tsukuba.ac.jp>
2019-05-17 18:02:20 +0900

    ログインができずにハマっている

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'eateries/new'
   post 'eateries/create'
 			      get 'lunch/go'
 			      root 'lunch#go'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to:'sessions#new'
+  post 'sessions/create'
+
 end
